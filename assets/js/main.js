b58ddda83a161c7dd41b7bb868a6eac300092f03
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-12-15 15:49:44 +0100

    1 milestone

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        arrayDischi: [],
+    },
+    mounted() {
+        axios.get('./api/index.php')
+            .then((res) => {
+
+                this.arrayDischi = res.data;
+
+            });
+    },
+    beforeUpdate() {
+
+    },
+    methods: {
+
+    }
+});
