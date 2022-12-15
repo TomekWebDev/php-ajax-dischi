5123f6428892e209048961c87e8512a3ee7e899c
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-12-15 16:56:39 +0100

    bonus

--- api/index.php
+++ api/index.php
@@ -3,8 +3,17 @@ include_once __DIR__ . '/../db/index.php';
 
 $arrayDischi = [];
 
-foreach ($database as $elem) {
-    $arrayDischi[] = $elem;
+if (!empty($_GET) && !empty($_GET['genre'])) {
+
+    foreach ($database as $elem) {
+
+        if ($elem['genre'] == $_GET['genre']) {
+
+            $arrayDischi[] = $elem;
+        }
+    }
+} else {
+    $arrayDischi = $database;
 }
 
 header('Content-type: application/json');
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         arrayDischi: [],
+        arrayGeneri: [],
+        selectGenere: ''
     },
     mounted() {
         axios.get('./api/index.php')
@@ -9,12 +11,28 @@ var app = new Vue({
 
                 this.arrayDischi = res.data;
 
+                this.arrayDischi.forEach((elem) => {
+
+                    // console.log(elem);
+
+
+                    if (!this.arrayGeneri.includes(elem.genre)) {
+                        this.arrayGeneri.push(elem.genre);
+                    }
+
+                });
+
             });
     },
     beforeUpdate() {
 
     },
     methods: {
-
+        ricercaAlbumPerGenere() {
+            axios.get(`./api/index.php?genre=${this.selectGenere}`)
+                .then((res) => {
+                    this.arrayDischi = res.data;
+                });
+        }
     }
 });
